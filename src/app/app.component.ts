df0bc8d0dce481e61443c9cddaee0f3d4129ad4d
arkajitroy <arkajitroy11.official@gmail.com>
2025-05-30 14:03:33 +0530

    learned: component + props passing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { getTodaysDateAndTime } from 'libs/date';
+import { getTodaysDateAndTime } from '../libs/date';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+})
+export class CardComponent {
+  @Input() title!: string;
+  @Input() description!: string;
+  @Input() tags: string[] = [];
+}
